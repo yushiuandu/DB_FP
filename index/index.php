24c0dfee7f5de4c1cd9a2493258baf8361b07e79
octopus0707 <fanny123456789@kimo.com>
2020-05-03 20:00:45 +0800

    2020 0503 張芳

--- index/index.php
+++ index/index.php
@@ -1,3 +1,9 @@
+<?php
+  $page = "index";
+  if(isset($_GET['page'])){
+    $page = $_GET['page'];
+  }
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -13,69 +19,128 @@
   </head>
   <body>
   <!-- 導覽列 -->
-  <nav class="navbar navbar-dark bg-info">
-    <a class="navbar-brand" href="#">
-      <img src="./image/title.png" width="32" high="32"> Tai-gun
-    </a>
-    <form class="form-inline search">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button type="button" class="btn btn-light" type="submit">Light</button>
-    </form>
-</nav>
+    <nav class="navbar navbar-default navbar-expend-lg head-nav">
+      <a class="navbar-brand" href="?page=index">
+        <img src="./image/title.png" width="32" high="32"> Tai-gun
+      </a>
+      <form class="form-inline search">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button type="button" class="btn btn-light" type="submit">Search</button>
+      </form>
+    </nav>
 <!-- 導覽列end -->
-  <!-- 左半部 -->
-    <div class=".col-3">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav  flex-column">
-        <li class="nav-item active">
-          	<a class="nav-link" href="#">所有看板</a>
-          	<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-				<a class="dropdown-item" href="#">美食版</a>
-				<a class="dropdown-item" href="#">美妝穿搭</a>
-				<a class="dropdown-item" href="#">旅遊版</a>
-				<a class="dropdown-item" href="#">新聞版</a>
-				<a class="dropdown-item" href="#">有趣版</a>
-				<a class="dropdown-item" href="#">感情版</a>
-				<a class="dropdown-item" href="#">其他版</a>
-          	</div>
-        </li>
-        <li class="nav-item">
-        	<a class="nav-link" href="#">訂閱看板</a>
-    		<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-        		<a class="dropdown-item" href="#">美食版</a>
-            	<a class="dropdown-item" href="#">美妝穿搭</a>
-            	<a class="dropdown-item" href="#">旅遊版</a>
-          	</div>
-        </li>
-        <li class="nav-item">
-			<a class="nav-link" href="#">熱門看板</a>
-			<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-				<a class="dropdown-item" href="#">有趣版</a>
-				<a class="dropdown-item" href="#">感情版</a>
-				<a class="dropdown-item" href="#">其他版</a>
-			</div>
-        </li>
-      </ul>
+  <div class="row">
+    <!-- 左半部 -->
+    <div class="col-12 col-md-2" id="left">
+      <nav class=" navbar-expand-lg navbar-default navbar-light" >
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav  flex-column">
+          <li class="nav-item">
+            <div class='link-head' id="all">
+              <div id="headingOne">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                所有看板</a>
+              </div>
+                <div id="collapseOne" class="collapse link-body" aria-label ledby="headingOne" data-parent="#all">
+                  <a class="dropdown-item" href="#">美食版</a>
+                  <a class="dropdown-item" href="#">美妝穿搭</a>
+                  <a class="dropdown-item" href="#">旅遊版</a>
+                  <a class="dropdown-item" href="#">新聞版</a>
+                  <a class="dropdown-item" href="#">有趣版</a>
+                  <a class="dropdown-item" href="#">感情版</a>
+                  <a class="dropdown-item" href="#">其他版</a>
+                </div>
+              </div>
+          </li>
+            <li class="nav-item dropdown">
+              <div class='link-head'>
+              <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                訂閱看板</a>
+                <div id="collapseOne" class="collapse link-body" aria-label ledby="headingOne" data-parent="#all">
+                  <a class="dropdown-item" href="#">美食版</a>
+                  <a class="dropdown-item" href="#">美妝穿搭</a>
+                  <a class="dropdown-item" href="#">旅遊版</a>
+                </div>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <div class='link-head'>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                熱門看板</a>
+                <div id="collapseOne" class="collapse link-body" aria-label ledby="headingOne" data-parent="#all">
+                  <a class="dropdown-item" href="#">有趣版</a>
+                  <a class="dropdown-item" href="#">感情版</a>
+                  <a class="dropdown-item" href="#">其他版</a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-</nav>
-  </div>
-  
-  </div>
   <!-- 中間(文章區) -->
-  <div class="container .col-6">
+    <div class="col-12 col-md-8" id="middle">
+	<?php if($page == 'index'):?>
+        <h1>article</h1>
       
-    
-        
-  </div>
+	<?php elseif($page == 'login'):?>
+	  	<?php include("login.php");?>
+	<?php endif;?>     
+    </div>
   <!-- 右半部 -->
-  <div class="container .col-3">
-      
-    
-        
+    <div class="col-12 col-md-2" id="right">
+      	<div id="login">
+			<!-- 如果登入了 -->
+			<!-- <div id="user-pic">
+				<img src="./image/test-user.jpg" class="img-fluid rounded-circle" alt="Responsive image" >
+			</div>
+			<div>
+				<p id='user-name' class="font-weight-bold">賀琬茹</p>
+				<p id='user-nickname'>小賀</p>
+			</div> -->
+			<!--如果沒有登入-->
+			<div id="user-pic">
+				<img src="./image/user.png" class="img-fluid rounded-circle" alt="Responsive image" >
+			</div>
+			<div >
+				<button type="button"class="btn btn-outline-info font-weight-bold" data-toggle="modal" data-target="#exampleModalCenter">註冊/登入</button>
+				
+				<div class="modal fade bd-example-modal-sm" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" >
+					<div class="modal-dialog modal-dialog-centered" role="document">
+						<div class="modal-content login-page">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLongTitle">walcome Tai-gun!</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<div>
+									<img src='./image/hello.png' id='login-pic' class='mar'>
+									<h5 class='mar'>帳號 <input type="account" placeholder="帳號" ></h5>
+									<h5 class='mar'>密碼 <input type="password" placeholder="密碼"></h5>
+									<button type="button" class="btn btn-secondary btn-sm mar"><a href='#'  style="color:white;">忘記密碼</button>
+									<button type="button" class="btn btn-secondary btn-sm mar"><a href="?page=login"  style="color:white;">還不是會員?</a></button>
+            		</div>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary font-weight-bold" data-dismiss="modal">下次再說</button>
+								<button class="btn btn-info font-weight-bold" type="submit">送出</button>
+							</div>
+						</div>
+  					</div>
+				</div>
+			</div>
+		</div>
+		<div id="other-page">
+      <button type="button" class="btn"><img src='./image/pencil.png'></button>
+      <button type="button" class="btn"><img src='./image/bell.png'></button>
+			<button type="button" class="btn"><img src='./image/test.png'></button>
+			<button type="button" class="btn"><img src='./image/friend.png'></button>
+		</div>
   </div>
 
     <!-- Optional JavaScript -->
